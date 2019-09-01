45b7affed13e3d7a2ec48a10583c6e4615358ffb
David <daveshuang@gmail.com>
2019-08-31 19:32:15 -0700

    Updates

---
+++ precache-manifest.4b47bcfc61901437f3a506342660c254.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ffdcfed048f908a7bb976257ab7c46",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c0ea40a9468979e603d2",
+    "url": "/clicky-game/static/css/main.a571cebd.chunk.css"
+  },
+  {
+    "revision": "27fe86deaba51a849ad9",
+    "url": "/clicky-game/static/js/2.f9292fcc.chunk.js"
+  },
+  {
+    "revision": "c0ea40a9468979e603d2",
+    "url": "/clicky-game/static/js/main.88a3b1dc.chunk.js"
+  },
+  {
+    "revision": "e900226b6c6e02320770",
+    "url": "/clicky-game/static/js/runtime~main.98b6a1fe.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/clicky-game/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/clicky-game/static/media/bg.9a30e6c6.png"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/clicky-game/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/clicky-game/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/clicky-game/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/clicky-game/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/clicky-game/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/clicky-game/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/clicky-game/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/clicky-game/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/clicky-game/static/media/poopy.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/clicky-game/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/clicky-game/static/media/summer.afdbb7bf.png"
+  }
+]);
